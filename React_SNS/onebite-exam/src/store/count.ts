c62f4bff8971744f998ec4ecae80fe9ff502341a
kkaengji <nkj960425@naver.com>
2026-02-03 10:46:10 +0900

    -

--- React_SNS/onebite-exam/src/store/count.ts
+++ React_SNS/onebite-exam/src/store/count.ts
@@ -3,20 +3,24 @@ import { create } from "zustand";
 
 type Store = {
   count: number;
-  increase: () => void;
-  decrease: () => void;
+  actions: {
+    increase: () => void;
+    decrease: () => void;
+  };
 };
 
 export const useCountStore = create<Store>((set, get) => ({
   count: 0,
-  increase: () => {
-    set((store) => ({
-      count: store.count + 1,
-    }));
-  },
-  decrease: () => {
-    set((store) => ({
-      count: store.count - 1,
-    }));
+  actions: {
+    increase: () => {
+      set((store) => ({
+        count: store.count + 1,
+      }));
+    },
+    decrease: () => {
+      set((store) => ({
+        count: store.count - 1,
+      }));
+    },
   },
 }));
